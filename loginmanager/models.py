53e57aebd31de1dcc5e0bd9c5f82079d15b93420
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2024-04-07 16:02:46 -0500

    Modelo del Login

--- loginmanager/models.py
+++ loginmanager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+    nombres = models.CharField(max_length = 50) 
+    apellidos = models.CharField(max_length = 50) 
+    pais =  models.CharField(max_length = 50) 
+    ciudad = models.CharField(max_length = 50) 
+    correo = models.CharField(max_length = 50) 
+    numero = models.CharField(max_length = 10)
+    def __str__(self):
+        return '%s %s' % (self.value, self.value)
