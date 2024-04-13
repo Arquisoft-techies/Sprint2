db86a469c53e341f717c49aef31b434e5073ae86
Maria Almeciga <malmeciga0104@gmail.com>
2024-04-12 22:42:15 -0500

    Completar lógica

--- authentication_orchestrator/logic/login_handler_logic.py
+++ authentication_orchestrator/logic/login_handler_logic.py
@@ -1,7 +1,7 @@
 # TODO: Arreglar import
-from otra_app_login.logic import process_request_login
+'''from otra_app_login.logic import process_request_login
 
 def send_request_to_login_handler(request_data):
     # TODO: Lógica para enviar la request al manejador de login: llamar a la función correspondiente en la otra aplicación
     response = process_request_login(request_data)
-    return response
+    return response'''
--- processes_orchestrator/logic/requests_handler_logic.py
+++ processes_orchestrator/logic/requests_handler_logic.py
@@ -1,7 +1,7 @@
-# TODO: Arreglar import
+'''# TODO: Arreglar import
 from otra_app_requests.logic import process_request_requests
 
 def send_request_to_requests_handler(request_data):
     # TODO: Lógica para enviar la request al manejador de requestes: llamar a la función correspondiente en la otra aplicación
     response = process_request_requests(request_data)
-    return response
+    return response'''
