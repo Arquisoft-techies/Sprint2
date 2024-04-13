63d1bed625b7bf2c5ef116e5ff93aaf70d1194e8
Maria Almeciga <malmeciga0104@gmail.com>
2024-04-12 20:06:51 -0500

    Cambios

--- authentication_orchestrator/views.py
+++ authentication_orchestrator/views.py
@@ -5,6 +5,18 @@ def offers_view(request: HttpRequest):
     if request.method == 'POST':
         request_data = request.POST.get('datos')  # Obtener datos de la request
 
+        # TODO: Recibir datos básicos que el usuario llena
+
+        basic_info = convertir_datos_JSON # Metodo de manejador Login
+
+        otp = generar_otp # TODO: Metodo de generacion otp en app otp
+
+        crear_datos_iniciales(basic_info, otp) # Metodo de manejador login
+
+
+
+
+
         # Determinar a qué manejador enviar la request
         request_type = determine_request_type(request_data)
         if request_type == 'login':
