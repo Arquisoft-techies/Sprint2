6bb5fecaf820fd98275090c694747576832ececd
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2024-04-13 08:13:22 -0500

    Cambio de base de datos

--- sprint/settings.py
+++ sprint/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'sprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psyciog2',
+        'NAME': 'sprint_db',
+        'USER': 'sprint_user',
+        'PASSWORD': 'isis2503',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
