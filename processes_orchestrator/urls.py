224bbe1d2df31c540e645d4a0969803a9bccffa6
Maria Almeciga <malmeciga0104@gmail.com>
2024-05-10 20:17:34 -0500

    Update urls.py

--- processes_orchestrator/urls.py
+++ processes_orchestrator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('proccesses/', views.offers_view, name='proccesses'),
+    #path('proccesses/', views.offers_view, name='proccesses'),
     # Otras URLs para otros types de requestes
 ]
