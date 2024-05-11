eb5ad559a06a019e4ce0e4ea28b3dcfd8fe0b6d8
Maria Almeciga <malmeciga0104@gmail.com>
2024-05-10 21:13:50 -0500

    fghjfg

--- processes_orchestrator/urls.py
+++ processes_orchestrator/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls import include
 
 urlpatterns = [
     #path('proccesses/', views.offers_view, name='proccesses'),
     # Otras URLs para otros types de requestes
+    path('solicitudes/', views.solicitud_list, name='solicitudList'),
+    path('solicitud/<id>', views.single_solicitud, name='singleSolicitud'),
+    path('solicitudCreate/', csrf_exempt(views.solicitud_create), name='crearSolicitud'),
 ]
