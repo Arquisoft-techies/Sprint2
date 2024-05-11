8e56a118e11f802af3cb4dee0c4d72f93399e099
Maria Almeciga <malmeciga0104@gmail.com>
2024-05-10 22:39:44 -0500

    Update urls.py

--- processes_orchestrator/urls.py
+++ processes_orchestrator/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import include
 urlpatterns = [
     #path('proccesses/', views.offers_view, name='proccesses'),
     # Otras URLs para otros types de requestes
-    path('solicitudes/', views.solicitud_list, name='solicitudList'),
+    path('solicitudes/', views.solicitud_list, name='solicitudes'),
     path('solicitud/<id>', views.single_solicitud, name='singleSolicitud'),
     path('crearSolicitud/', csrf_exempt(views.solicitud_create), name='crearSolicitud'),
-    path('aprobarSolicitud/', csrf_exempt(views.solicitud_approve), name='aprobarSolicitud'),
+    path('aprobarSolicitud', csrf_exempt(views.solicitud_approve), name='aprobarSolicitud'),
 ]
